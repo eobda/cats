50538b14e358931dc3bbcd80981855150a18fb29
maddie <lee.maddie@gmail.com>
2023-09-16 00:23:47 -0400

    Linted code and cleaned up for style

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -5,15 +5,12 @@ const breedDetailsFromFile = function(breed, functionWhenDone) {
   fs.readFile(`./data/${breed}.txt`, 'utf-8', (error, data) => {
     console.log("In readFile's callback: it has the data.");
 
-    if (!error) {
-      functionWhenDone(data);
-    }
+    if (!error) functionWhenDone(data);
   });
-  // Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-const printData = function(breed) {
+const printData = breed => {
   console.log('Return Value:', breed);
-}
+};
 
 breedDetailsFromFile('Bombay', printData);
