9bcbfb4c27811a8db19fbccd2b89f5582b193eeb
maddie <lee.maddie@gmail.com>
2023-09-16 00:22:53 -0400

    Implemented callback

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, functionWhenDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf-8', (error, data) => {
     console.log("In readFile's callback: it has the data.");
 
-    if (!error) return data;
+    if (!error) {
+      functionWhenDone(data);
+    }
   });
   // Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
 };
 
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value:', bombay);
+const printData = function(breed) {
+  console.log('Return Value:', breed);
+}
+
+breedDetailsFromFile('Bombay', printData);
